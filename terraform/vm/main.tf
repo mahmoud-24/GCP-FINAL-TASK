8d5a7e0641a7be65c1316c8a8146e31bd15af9dd
mahmoud-24 <mahmoud.ibrahim2411@gmail.com>
2023-01-24 23:52:34 +0200

    second push (infrasture code)

---
+++ terraform/vm/main.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "private-vm" {
+  name         = "private-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
+/*
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+}
+*/
